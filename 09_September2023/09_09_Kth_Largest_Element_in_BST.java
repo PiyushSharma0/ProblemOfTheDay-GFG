e07fff7582f0960e3c0fb9392727309c6ec37dc7
Piyush Sharma <piyush.sharma.code@gmail.com>
2023-09-09 23:50:19 +0530

    Day 106

---
+++ 09_September2023/09_09_Kth_Largest_Element_in_BST.java
@@ -0,0 +1,45 @@
+// Link: https://practice.geeksforgeeks.org/problems/kth-largest-element-in-bst/1
+// Time Complexity: O(H + K) where H is the height of the tree and K is the input
+// Space Complexity: O(H) where H is the height of the tree
+
+class Solution
+{
+    // Create a class variable to keep track of the count
+    private int count = 0;
+    
+    // Create a variable to store the Kth largest element
+    private int kthLargestElement = -1;
+
+    // Helper function to perform reverse in-order traversal
+    private void reverseInOrderTraversal(Node root, int K)
+    {
+        if (root == null || count >= K) {
+            return;
+        }
+
+        // Recur on the right subtree first
+        reverseInOrderTraversal(root.right, K);
+
+        // Increment the count
+        count++;
+
+        // If the count reaches K, store the Kth largest element
+        if (count == K) {
+            kthLargestElement = root.data;
+            return;
+        }
+
+        // Recur on the left subtree
+        reverseInOrderTraversal(root.left, K);
+    }
+
+    // Function to find the Kth largest element in the BST
+    public int kthLargest(Node root, int K)
+    {
+        // Start the reverse in-order traversal
+        reverseInOrderTraversal(root, K);
+
+        // Return the Kth largest element
+        return kthLargestElement;
+    }
+}
