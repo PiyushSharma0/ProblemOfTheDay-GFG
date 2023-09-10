af3301e609bf9125ba0ea9c6dd70fce7cafef963
Piyush Sharma <piyush.sharma.code@gmail.com>
2023-09-10 23:59:04 +0530

    Day 107

---
+++ 09_September2023/10_09_Insert_a_Node_in_BST.java
@@ -0,0 +1,37 @@
+// Link: https://practice.geeksforgeeks.org/problems/insert-a-node-in-a-bst/
+// Time Complexity: O(h) where h is the height of the BST.
+// Space Complexity: O(h) where h is the height of the BST.
+
+class Solution {
+    // Definition for a binary tree node.
+    class Node {
+        int data;
+        Node left, right;
+        
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    // Function to insert a node in a BST.
+    Node insert(Node root, int Key) {
+        // If the tree is empty, create a new node and return it as the new root.
+        if (root == null) {
+            root = new Node(Key);
+            return root;
+        }
+
+        // If the key is less than the root's data, recursively insert into the left subtree.
+        if (Key < root.data) {
+            root.left = insert(root.left, Key);
+        }
+        // If the key is greater than the root's data, recursively insert into the right subtree.
+        else if (Key > root.data) {
+            root.right = insert(root.right, Key);
+        }
+
+        // Return the (unchanged) node pointer.
+        return root;
+    }
+}
