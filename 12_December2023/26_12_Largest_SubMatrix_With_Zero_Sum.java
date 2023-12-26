a60c61125f25a16ab9487b71bd957eb634bbda25
Piyush Sharma <piyush.sharma.code@gmail.com>
2023-12-26 21:06:33 +0530

    Day 214

--- 12_December2023/26_12_Largest_SubMatrix_With_Zero_Sum.java
+++ 12_December2023/26_12_Largest_SubMatrix_With_Zero_Sum.java
@@ -1,6 +1,11 @@
+// Link: https://practice.geeksforgeeks.org/problems/largest-sub-matrix-with-0-sum/0
+// Time Complexity: O(n^4)
+// Space Complexity: O(n^2)
+
 import java.util.ArrayList;
 
 class Solution {
+    // Coord class to represent coordinates of a rectangle
     static class Coord {
         int x1, y1, x2, y2;
 
@@ -12,32 +17,39 @@ class Solution {
         }
     }
 
+    // Method to find the largest rectangular sub-matrix with a sum of 0
     public static ArrayList<ArrayList<Integer>> sumZeroMatrix(int[][] a) {
         int n = a.length;
         int m = a[0].length;
-        int[][] pre = new int[n][m];
 
+        // Calculate prefix sum matrix
+        int[][] pre = new int[n][m];
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < m; ++j)
                 pre[i][j] = a[i][j];
 
+        // Compute cumulative sums along rows
         for (int i = 1; i < n; ++i)
             for (int j = 0; j < m; ++j)
                 pre[i][j] += pre[i - 1][j];
 
+        // Compute cumulative sums along columns
         for (int i = 0; i < n; ++i)
             for (int j = 1; j < m; ++j)
                 pre[i][j] += pre[i][j - 1];
 
+        // Variables to store information about the sub-matrix with sum 0
         Coord maxCoord = new Coord(-1, -1, -1, -1);
         int area = 0;
 
+        // Iterate through all possible sub-matrices
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
                 int sum = 0;
                 if (i > 0 && j > 0)
                     sum += pre[i - 1][j - 1];
 
+                // Explore all possible rectangles with the current (i, j) as the top-left corner
                 for (int y = m - 1; y >= j; --y) {
                     for (int x = n - 1; x >= i; --x) {
                         if (area <= (x - i + 1) * (y - j + 1)) {
@@ -47,6 +59,7 @@ class Solution {
                             if (j > 0)
                                 s -= pre[x][j - 1];
 
+                            // Check if the sum is 0 and update maxCoord accordingly
                             if (s == 0) {
                                 if (area != (x - i + 1) * (y - j + 1)) {
                                     area = (x - i + 1) * (y - j + 1);
@@ -71,6 +84,7 @@ class Solution {
             }
         }
 
+        // Construct the result matrix based on maxCoord
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         if (area == 0)
             return result;
