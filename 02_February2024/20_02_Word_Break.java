370af6983129640d1e8927a5d55af55954dbc582
Piyush Sharma <piyush.sharma.code@gmail.com>
2024-02-20 23:54:02 +0530

    Day 270

---
+++ 02_February2024/20_02_Word_Break.java
@@ -0,0 +1,26 @@
+// Link: https://practice.geeksforgeeks.org/problems/word-break1352/1
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+
+class Solution
+{
+    public static int wordBreak(int n, String s, ArrayList<String> dictionary )
+    {
+        //Base case
+        if(s.length() == 0)return 0;
+        
+        int N = s.length();
+        boolean[] dp = new boolean[N+1];
+        dp[0] = true;
+        
+        for(int i=0; i<=N; i++){
+            for(int j=0; j<=i; j++){
+                if(dp[j]&&dictionary.contains(s.substring(j,i))){
+                    dp[i]=true;
+                }
+            }
+        }
+
+        return dp[N]?1:0;
+    }
+}
